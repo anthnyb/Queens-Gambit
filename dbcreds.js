3f2c3e9fe117116d38858d568551b5ae4abb693a
anthnyb <anthony.benites17@gmail.com>
2022-06-27 21:26:11 -0700

    update gitignore

--- dbcreds.js
+++ dbcreds.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-    user: "", //enter your postgres user
-    password: "", //enter your postgres password
+    user: "postgres", //enter your postgres user
+    password: process.env.PG_ROOT, //enter your postgres password
     host: "localhost",
     port: 5432,
-    database: "" //enter the name of the created database
+    database: "chessdb" //enter the name of the created database
 };
